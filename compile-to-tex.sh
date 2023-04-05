d70d669ca212e6e9b86f8a01673f7b9c0e3e65dd
Eli Adelhult <eli.adelhult@outlook.com>
2023-04-05 11:13:36 +0200

    Improved build process

--- compile-to-tex.sh
+++ compile-to-tex.sh
@@ -1,3 +1,5 @@
+modmark="cargo run --release --manifest-path ${MODMARK_PATH} --"
+
 cargo build --manifest-path chalmers-thesis/Cargo.toml --release --target wasm32-wasi
 cp chalmers-thesis/target/wasm32-wasi/release/chalmers-thesis.wasm ./
-./modmark --assets "assets" --allow-every-module main.mdm out/thesis.tex
+$modmark --assets "assets" --allow-every-module main.mdm out/thesis.tex
---
+++ live-preview.sh
@@ -0,0 +1,2 @@
+modmark="cargo run --release --manifest-path ${MODMARK_PATH} --"
+$modmark --assets assets -A main.mdm
