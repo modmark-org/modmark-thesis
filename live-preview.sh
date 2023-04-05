fd33909171dfea5f83ec05f7e90ffe9cfcc4fae0
Eli Adelhult <eli.adelhult@outlook.com>
2023-04-05 11:15:28 +0200

    Build chalmers-thesis package when running live preview too

--- live-preview.sh
+++ live-preview.sh
@@ -1,2 +1,6 @@
 modmark="cargo run --release --manifest-path ${MODMARK_PATH} --"
+
+cargo build --manifest-path chalmers-thesis/Cargo.toml --release --target wasm32-wasi
+cp chalmers-thesis/target/wasm32-wasi/release/chalmers-thesis.wasm ./
+
 $modmark --assets assets -A main.mdm
