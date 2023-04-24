d7869847e6f91489cb32e7fb4f35553653024058
Eli Adelhult <eli.adelhult@outlook.com>
2023-04-24 10:59:16 +0200

    Restructure repo (to ensure that figures are found by latex)

--- compile-to-tex.sh
+++ compile-to-tex.sh
@@ -2,4 +2,4 @@ modmark="cargo run --release --manifest-path ${MODMARK_PATH} --"
 
 cargo build --manifest-path chalmers-thesis/Cargo.toml --release --target wasm32-wasi
 cp chalmers-thesis/target/wasm32-wasi/release/chalmers-thesis.wasm ./
-$modmark compile --assets "assets" --allow-every-module main.mdm out/thesis.tex
+$modmark compile --assets "." --allow-every-module main.mdm thesis.tex
--- live-preview.sh
+++ live-preview.sh
@@ -2,5 +2,4 @@ modmark="cargo run --release --manifest-path ${MODMARK_PATH} --"
 
 cargo build --manifest-path chalmers-thesis/Cargo.toml --release --target wasm32-wasi
 cp chalmers-thesis/target/wasm32-wasi/release/chalmers-thesis.wasm ./
-
-$modmark compile --assets assets -A main.mdm
+$modmark compile --assets "." -A main.mdm
