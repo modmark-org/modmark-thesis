52426f31473abee6308d53cd116598445081fd41
Eli Adelhult <eli.adelhult@outlook.com>
2023-05-11 09:51:10 +0200

    Small fix

--- compile-to-tex.sh
+++ compile-to-tex.sh
@@ -1,4 +1,4 @@
-modmark="cargo run --release --manifest-path ${MODMARK_PATH} --"
+modmark="cargo run --release --manifest-path ${MODMARK_PATH}/cli/Cargo.toml --"
 
 cargo build --manifest-path chalmers-thesis/Cargo.toml --release --target wasm32-wasi
 cp chalmers-thesis/target/wasm32-wasi/release/chalmers-thesis.wasm ./
